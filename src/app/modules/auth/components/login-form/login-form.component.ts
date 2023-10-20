14ca5762b74f5e0bcaceda221a6a3534a314fec4
Jenny Aguilar <contact@jaguilarweb.com>
2023-10-20 12:58:47 +0200

    refactor: typed status and add error message

--- src/app/modules/auth/components/login-form/login-form.component.ts
+++ src/app/modules/auth/components/login-form/login-form.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faPen, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
+import { AuthService } from '@services/auth.service';
+
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html'
@@ -17,18 +19,28 @@ export class LoginFormComponent {
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
-  status: string = 'init';
+  status: 'init' | 'loading' | 'success' | 'failed' = 'init';
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   doLogin() {
     if (this.form.valid) {
       this.status = 'loading';
       const { email, password } = this.form.getRawValue();
-      // TODO
+      this.authService.login(email, password)
+      .subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/app']);
+        },
+        error: () => {
+          this.status = 'failed';
+        }
+      });
     } else {
       this.form.markAllAsTouched();
     }
