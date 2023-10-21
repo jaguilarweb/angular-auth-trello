7e1098b50aa1158e03478898497c2d8e2a2e8c6c
Jenny Aguilar <contact@jaguilarweb.com>
2023-10-21 09:45:36 +0200

    feat: Add user register method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -21,4 +21,12 @@ export class AuthService {
     });
   }
 
+  register(name: string,  email: string, password: string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/register`, {
+      name,
+      email,
+      password
+    });
+  }
+
 }
