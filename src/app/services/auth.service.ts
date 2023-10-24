9797a3cbb117d835e9562fdea274c0900d40f578
Jenny Aguilar <contact@jaguilarweb.com>
2023-10-24 10:17:46 +0200

    feat: Add cookies management and logout method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -59,4 +59,8 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/api/v1/auth/change-password`, {token, newPassword})
   }
 
+  logout() {
+    this.tokenService.removeToken();
+  }
+
 }
--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { getCookie, setCookie, removeCookie } from 'typescript-cookie';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,16 +10,17 @@ export class TokenService {
   constructor() { }
 
   saveToken(token: string){
-    localStorage.setItem('token', token);
+    //Expires: Fecha de validez de la cookie, no del token.
+    setCookie('token-trello', token, { expires: 365, path: '/'});
   }
 
   getToken(){
-    const token = localStorage.getItem('token');
+    const token = getCookie('token-trello');
     return token;
   }
 
   removeToken(){
-    localStorage.removeItem('token');
+    removeCookie('token-trello');
   }
 
 
